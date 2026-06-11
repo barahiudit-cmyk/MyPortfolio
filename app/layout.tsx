7e8381da7bf73ef5ede3819bcc8ad54f35964294
thakurudit <thakurudit@gmail.com>
2026-06-11 17:10:52 +0530

    for add new things in myproject section

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,12 +34,12 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+
       <body className="min-h-full flex flex-col">
-        
+
         {children}
-       
+
       </body>
     </html>
   );
